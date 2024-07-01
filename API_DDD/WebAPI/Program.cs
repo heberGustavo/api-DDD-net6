01dab56ba7ff63e55776938e922dd27c994baab2
heberGustavo <heber.gbarbosa@hotmail.com>
2024-07-01 13:33:07 -0300

    Configure Program class

--- API_DDD/WebAPI/Program.cs
+++ API_DDD/WebAPI/Program.cs
@@ -1,3 +1,18 @@
+using AutoMapper;
+using Domain.Interfaces;
+using Domain.Interfaces.Generics;
+using Domain.Interfaces.InterfaceServices;
+using Domain.Services;
+using Entities.Entities;
+using Infrastructure.Configuration;
+using Infrastructure.Repository.Generics;
+using Infrastructure.Repository.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using WebAPI.Models;
+using WebAPI.Token;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,19 +22,113 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#region Conf. Services
+
+builder.Services.AddDbContext<ContextBase>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
+
+builder.Services
+	.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+	.AddEntityFrameworkStores<ContextBase>();
+
+#endregion
+
+#region Dependency Injection
+
+#region Domain
+
+builder.Services.AddSingleton<IServiceMessage, ServiceMessage>();
+
+#endregion
+
+#region Repository
+
+builder.Services.AddSingleton(typeof(IGeneric<>), typeof(RepositoryGeneric<>));
+builder.Services.AddSingleton<IMessage, RepositoryMessage>();
+
+#endregion
+
+#endregion
+
+#region JWT
+
+builder.Services
+	.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(option =>
+	{
+		option.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuer = false,
+			ValidateAudience = false,
+			ValidateLifetime = true,
+			ValidateIssuerSigningKey = true,
+
+			ValidIssuer = "Company.Securiry.Bearer",
+			ValidAudience = "Company.Securiry.Bearer",
+			IssuerSigningKey = JwtSecurityKey.Create("Secret_Key-12345678_example")
+		};
+
+		option.Events = new JwtBearerEvents
+		{
+			OnAuthenticationFailed = context =>
+			{
+				Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+				return Task.CompletedTask;
+			},
+			OnTokenValidated = context =>
+			{
+				Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+				return Task.CompletedTask;
+			}
+		};
+	});
+
+#endregion
+
+#region AutoMapper
+
+var config = new AutoMapper.MapperConfiguration(cfg =>
+{
+	cfg.CreateMap<MessageViewModel, Message>();
+	cfg.CreateMap<Message, MessageViewModel>();
+});
+
+IMapper mapper = config.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+#endregion
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
 
+#region CORS
+
+//var urlDev = "https://dominiodocliente.com.br";
+//var urlHML = "https://dominiodocliente2.com.br";
+//var urlPROD = "https://dominiodocliente3.com.br";
+//app.UseCors(b => b.WithOrigins(urlDev, urlHML, urlPROD));
+
+var devClient = "https://localhost:7185";
+app.UseCors(x => x
+.AllowAnyOrigin()
+.AllowAnyMethod()
+.AllowAnyHeader().WithOrigins(devClient));
+
+#endregion
+
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseSwaggerUI();
 
 app.Run();

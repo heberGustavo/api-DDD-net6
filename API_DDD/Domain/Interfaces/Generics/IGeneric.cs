3d768d752071a33e24f8dcfe330eb4ae96dfe97e
heberGustavo <heber.gbarbosa@hotmail.com>
2024-07-01 06:56:13 -0300

    Create implementation to Domain

---
+++ API_DDD/Domain/Interfaces/Generics/IGeneric.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Interfaces.Generics
+{
+	public interface IGeneric<T> where T : class
+	{
+		Task Add(T Objeto);
+		Task Update(T Objeto);
+		Task Remove(T Objeto);
+		Task<T> GetEntityById(int Id);
+		Task<List<T>> List();
+	}
+}

4b94b8ec01f6f2ffe455b930f90a21973236c523
heberGustavo <heber.gbarbosa@hotmail.com>
2024-07-01 06:42:55 -0300

    Create entities

---
+++ API_DDD/Entities/Entities/Notifies.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Entities
+{
+	public class Notifies
+	{
+        public Notifies()
+        {
+            Notificacoes = new List<Notifies>();
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+		[NotMapped]
+		public string Mensagem { get; set; }
+		
+        [NotMapped]
+		public List<Notifies> Notificacoes { get; set; }
+
+		#region Métodos Publicos
+
+		public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+		{
+			if(string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+			{
+				Notificacoes.Add(new Notifies
+				{
+					Mensagem = "Campo Obrigatório",
+					NomePropriedade = nomePropriedade
+				});
+
+				return false;
+			}
+
+			return true;
+		}
+
+		public bool ValidarPropriedadeInt(int valor, string nomePropriedade)
+		{
+			if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+			{
+				Notificacoes.Add(new Notifies
+				{
+					Mensagem = "Campo Obrigatório",
+					NomePropriedade = nomePropriedade
+				});
+
+				return false;
+			}
+
+			return true;
+		}
+
+		#endregion
+	}
+}

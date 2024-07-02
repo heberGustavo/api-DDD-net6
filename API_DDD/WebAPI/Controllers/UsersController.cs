585a80bd225b551400e508b344a6df058cc31f61
heberGustavo <heber.gbarbosa@hotmail.com>
2024-07-02 15:45:54 -0300

    Create UserController and deployment method to authenticate and create a new user

---
+++ API_DDD/WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,90 @@
+﻿using Entities.Entities;
+using Entities.Enum;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using WebAPI.Models;
+using WebAPI.Token;
+
+namespace WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersController : ControllerBase
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly SignInManager<ApplicationUser> _signInManager;
+
+		public UsersController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		[AllowAnonymous]
+		[Produces("application/json")]
+		[HttpPost("/api/Autenticar")]
+		public async Task<IActionResult> Autenticar([FromBody] Login login)
+		{
+			if(login == null || string.IsNullOrWhiteSpace(login.email) || string.IsNullOrWhiteSpace(login.senha)) 
+				return Unauthorized();
+
+			var result = await _signInManager.PasswordSignInAsync(login.email, login.senha, false, lockoutOnFailure: false);
+			if (result.Succeeded)
+			{
+				var userCurrent = await _userManager.FindByEmailAsync(login.email);
+
+				var token = new TokenJWTBuilder()
+					.AddSecurityKey(JwtSecurityKey.Create("Secret_Key-123456789"))
+					.AddSubject("Empresa - Heber Gustavo .NET")
+					.AddIssuer("Empresa.Teste.Security.Bearer")
+					.AddAudience("Empresa.Teste.Security.Bearer")
+					.AddClaim("IdUsuario", userCurrent.Id)
+					.AddExpiry(5)
+					.Builder();
+
+				return Ok(token.value);
+			}
+
+			return Unauthorized();
+		}
+
+		[AllowAnonymous]
+		[Produces("application/json")]
+		[HttpPost("/api/CadastrarUsuario")]
+		public async Task<IActionResult> CadastrarUsuario([FromBody] Login login)
+		{
+			if (login == null || string.IsNullOrWhiteSpace(login.email) || string.IsNullOrWhiteSpace(login.senha))
+				return BadRequest("Necessário informar Email e Senha");
+
+			var user = new ApplicationUser
+			{
+				UserName = login.email,
+				Email = login.senha,
+				CPF = login.cpf,
+				Tipo = TipoUsuario.Comum
+			};
+
+			var resultCreate = await _userManager.CreateAsync(user, login.senha);
+			if (resultCreate.Errors.Any())
+				return BadRequest(resultCreate.Errors);
+
+			// Geração de Confirmação, caso precise
+			var userId = await _userManager.GetUserIdAsync(user);
+			var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+			code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+			// retorno email
+			code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+			var resultado2 = await _userManager.ConfirmEmailAsync(user, code);
+			
+			if (resultado2.Succeeded)
+				return Ok("Usuário Adicionado com Sucesso");
+			else
+				return Ok("Erro ao confirmar usuários");
+		}
+	}
+}
